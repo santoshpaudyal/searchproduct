22940c78e14f6fe9a47c6d870f4cceed020bd27a
Santosh Paudyal <santoshpaudyal99@gmail.com>
2020-01-06 13:58:53 +0545

    first initialize

---
+++ product/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import SearchItem
+
+# Register your models here.
+admin.site.register(SearchItem)
---
+++ readme.md
@@ -0,0 +1,1 @@
+Web scrappying using Django Framework
